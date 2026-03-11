5a7ccb668bf7cecdeae3b8796a339b941f07441d
Quaked <124531365+QuakedK@users.noreply.github.com>
2026-03-11 15:21:38 -0500

    Add files via upload

---
+++ Downloads/Nip Import.bat
@@ -0,0 +1,35 @@
+:: Made by Quaked
+:: TikTok: _Quaked_
+:: Discord: https://discord.gg/8NqDSMzYun
+
+@echo off
+
+:: Checking if OSU Tools exist. 
+if not exist "C:\OSU Tools" (
+   goto :Download_Tools 
+) else (
+   goto :Import_Nip 
+)
+
+:: Download Nvidia Profile Inspector + Custom OSU Nip.
+:Download_Tools
+set "FileURL=https://github.com/QuakedK/Osu-Optimizer/raw/refs/heads/main/Downloads/OSUTools.zip"
+set "FileName=OSU Tools.zip"
+set "ExtractFolder=C:\OSU Tools"
+set "DownloadsFolder=C:\"
+curl -s -L "%FileURL%" -o "%DownloadsFolder%\%FileName%"
+if exist "%DownloadsFolder%\%FileName%" (  
+   mkdir "%ExtractFolder%" >nul 2>&1
+   pushd "%ExtractFolder%" >nul 2>&1
+   tar -xf "%DownloadsFolder%\%FileName%" --strip-components=1 >nul 2>&1
+   popd >nul 2>&1
+   del /q "C:\OSU Tools.zip" >nul 2>&1
+   goto :Import_Nip
+) else (
+   goto :Download_Tools
+)
+
+:: Import Custom OSU Nip.
+:Import_Nip
+"C:\OSU Tools\nvidiaProfileInspector.exe" -silentImport "C:\OSU Tools\OSU.nip" >nul 2>&1
+exit
